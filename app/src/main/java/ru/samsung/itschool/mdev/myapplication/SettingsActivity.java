4a5471af9a2932bd2b10a48db03aa22efb45b133
Pavel <impetus@yandex.ru>
2022-11-16 18:04:19 +0300

    Initial commit

---
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/SettingsActivity.java
@@ -0,0 +1,43 @@
+package ru.samsung.itschool.mdev.myapplication;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import java.util.Map;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Map<String,?> allentries = sharedPreferences.getAll();
+        for(Map.Entry<String, ?> entry: allentries.entrySet()) {
+            Log.i("RRR",entry.getKey() + " => " + entry.getValue());
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
